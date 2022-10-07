31062fc7423f1795cd5851e67eb54867ba804ef3
Siddhartha V <106438627+siddharthav19@users.noreply.github.com>
2022-10-07 12:26:39 +0530

    Update search2DMatrix.cpp

--- Algorithms/C/searching/search2DMatrix.cpp
+++ Algorithms/C/searching/search2DMatrix.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+	//start at the right most in the starting row -> i,j implies the starting point
+	//if the element at matrix[i][j] is equal to target return true
+	//else if it's less than target increment i (assures that target wont be in current row) as every element is sorted in row wise order and column wise order
+	//else if it's greater than target decrement j (assures that target wont be in current column
+	
 	
 	int row = matrix.size();
 	int col = matrix[0].size();
@@ -30,6 +36,7 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
 int main()
 {
-	vector<int> v;
+	vector<vector<int>> v= { { 1, 3, 5 }, { 10, 11, 16 }, { 24, 36, 37 } };
+	cout<< searchMatrix(v,11)<<endl;
 	
 }
